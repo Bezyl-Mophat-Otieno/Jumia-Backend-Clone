96b11dbbca3c5c2e2becb7a4e7bbbed1ad114fcf
Bezyl Mophat Otieno <bezyl.otieno@griffinglobaltech.com>
2024-01-01 10:43:23 +0300

    Updated the Email Service to be sending emails from the stacks for all the Payments Validated .

--- EmailMS/Messaging/AzureServiceBusConsumer.cs
+++ EmailMS/Messaging/AzureServiceBusConsumer.cs
@@ -14,9 +14,11 @@ namespace EmailMS.Messaging
         private readonly string _connstring;
 
         private readonly string _queuname;
+        private readonly string _stackname;
 
         private readonly IConfiguration _configuration;
         private readonly ServiceBusProcessor _emailprocessor;
+        private readonly ServiceBusProcessor _paymentemailprocessor;
         private readonly EmailService _emailservice;
 
 
@@ -26,10 +28,13 @@ namespace EmailMS.Messaging
 
             _connstring = _configuration.GetValue<string>("Azureconnectionstring");
             _queuname = _configuration.GetValue<string>("QueueandTopics:registerqueue");
+            _stackname = _configuration.GetValue<string>("paymentverification:paymentmade");
+
 
             var client = new ServiceBusClient(_connstring);
 
             _emailprocessor = client.CreateProcessor(_queuname);
+            _paymentemailprocessor = client.CreateProcessor(_stackname, "EmailService");
             _emailservice = new EmailService(configuration);
 
 
@@ -37,9 +42,44 @@ namespace EmailMS.Messaging
         }
         public async Task Start()
         {
+            
+            _paymentemailprocessor.ProcessMessageAsync += OnPaymentVerification;
+            _paymentemailprocessor.ProcessErrorAsync += ErrorHandler;
+            await _paymentemailprocessor.StartProcessingAsync();
             _emailprocessor.ProcessMessageAsync += OnRegisterUser;
             _emailprocessor.ProcessErrorAsync += ErrorHandler;
             await _emailprocessor.StartProcessingAsync();
+        }
+
+        private async Task OnPaymentVerification(ProcessMessageEventArgs args)
+        {
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+            var user = JsonConvert.DeserializeObject<UserMessageDTO>(body);
+
+            try
+            {
+                // Sending the Mail
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.Append("<h1> Hello " + user.Name + "</h1>");
+                stringBuilder.AppendLine("<br/> Your Order has been Recieved");
+
+                stringBuilder.Append("<br/>");
+                stringBuilder.Append('\n');
+
+                await _emailservice.SendMail(user, stringBuilder.ToString());
+
+                // When Done
+                await args.CompleteMessageAsync(args.Message); // we are done delete the message from the Stack
+
+            }
+            catch (Exception ex)
+            {
+
+                // send mail to admin
+                Console.WriteLine(ex.Message);
+                throw;
+            }
         }
 
         private Task ErrorHandler(ProcessErrorEventArgs args)
@@ -85,6 +125,8 @@ namespace EmailMS.Messaging
         public async  Task Stop()
         {
             await _emailprocessor.StopProcessingAsync();
+            await _paymentemailprocessor.StopProcessingAsync();
+            await _paymentemailprocessor.StartProcessingAsync();
             await _emailprocessor.DisposeAsync();
         }
     }
